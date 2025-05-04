a8fbb2fdfcecfe0917219c2fb0ad2ce5cd0c7ae8
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-05-03 22:48:15 -0300

    constans(task): add task enums and task entity

---
+++ src/entities/task.entity.ts
@@ -0,0 +1,8 @@
+export class TaskEntity {
+    id?: number;
+    title: string;
+    description: string;
+    status?: string;
+    create_at?: Date;
+    updated_at?: Date;
+}
---
+++ src/enums/di-tokens/task/task-enum.ts
@@ -0,0 +1,6 @@
+export enum TaskEnum {
+    TO_DO = 'TO_DO',
+    IN_PROGRESS = 'IN_PROGRESS',
+    COMPLETED = 'COMPLETED',
+    DELETED = 'DELETED',
+}
